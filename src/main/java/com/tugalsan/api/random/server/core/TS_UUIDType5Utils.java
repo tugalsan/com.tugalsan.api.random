00398f9e6bcbae9f834f6b58dfa08838bdefa1d4
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-01-26 01:46:58 +0300

    .

--- src/main/java/com/tugalsan/api/random/server/core/TS_UUIDType5Utils.java
+++ src/main/java/com/tugalsan/api/random/server/core/TS_UUIDType5Utils.java
@@ -1,7 +1,7 @@
 package com.tugalsan.api.random.server.core;
 
 import com.tugalsan.api.unsafe.client.*;
-import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.util.Objects;
 import java.util.UUID;
@@ -12,33 +12,31 @@ public class TS_UUIDType5Utils {
 //        System.out.println(TS_UUIDType5Utils.nameUUIDFromNamespaceAndString(NAMESPACE_URL, "google.com"));
 //        System.out.println(TS_UUIDType5Utils.nameUUIDFromNamespaceAndString(NAMESPACE_URL, "google.com"));
 //        System.out.println(TS_UUIDType5Utils.nameUUIDFromNamespaceAndString(NAMESPACE_URL, "google.com"));
-//        var a = run("Melih Köse 2");
+//        var a = run("AA BB 2");
 //        System.out.println(a);
 //    }
     public static UUID run(CharSequence name) {
         return nameUUIDFromNamespaceAndString(getNAMESPACE_URL(), name);
     }
 
-    private static final Charset UTF8 = Charset.forName("UTF-8");
+//    private static final UUID getNAMESPACE_DNS() {
+//        return UUID.fromString("6ba7b810-9dad-11d1-80b4-00c04fd430c8");
+//    }
 
-    private static final UUID getNAMESPACE_DNS() {
-        return UUID.fromString("6ba7b810-9dad-11d1-80b4-00c04fd430c8");
-    }
-
-    private static final UUID getNAMESPACE_URL() {
+    private static UUID getNAMESPACE_URL() {
         return UUID.fromString("6ba7b811-9dad-11d1-80b4-00c04fd430c8");
     }
 
-    private static final UUID getNAMESPACE_OID() {
-        return UUID.fromString("6ba7b812-9dad-11d1-80b4-00c04fd430c8");
-    }
-
-    private static final UUID getNAMESPACE_X500() {
-        return UUID.fromString("6ba7b814-9dad-11d1-80b4-00c04fd430c8");
-    }
+//    private static final UUID getNAMESPACE_OID() {
+//        return UUID.fromString("6ba7b812-9dad-11d1-80b4-00c04fd430c8");
+//    }
+//
+//    private static final UUID getNAMESPACE_X500() {
+//        return UUID.fromString("6ba7b814-9dad-11d1-80b4-00c04fd430c8");
+//    }
 
     private static UUID nameUUIDFromNamespaceAndString(UUID namespace, CharSequence name) {
-        return nameUUIDFromNamespaceAndBytes(namespace, Objects.requireNonNull(name.toString(), "name == null").getBytes(UTF8));
+        return nameUUIDFromNamespaceAndBytes(namespace, Objects.requireNonNull(name.toString(), "name == null").getBytes(StandardCharsets.UTF_8));
     }
 
     private static UUID nameUUIDFromNamespaceAndBytes(UUID namespace, byte[] name) {
@@ -62,10 +60,10 @@ public class TS_UUIDType5Utils {
         var msb = 0L;
         var lsb = 0L;
         assert data.length >= 16;
-        for (int i = 0; i < 8; i++) {
+        for (var i = 0; i < 8; i++) {
             msb = (msb << 8) | (data[i] & 0xff);
         }
-        for (int i = 8; i < 16; i++) {
+        for (var i = 8; i < 16; i++) {
             lsb = (lsb << 8) | (data[i] & 0xff);
         }
         return new UUID(msb, lsb);
@@ -76,10 +74,10 @@ public class TS_UUIDType5Utils {
         var out = new byte[16];
         var msb = uuid.getMostSignificantBits();
         var lsb = uuid.getLeastSignificantBits();
-        for (int i = 0; i < 8; i++) {
+        for (var i = 0; i < 8; i++) {
             out[i] = (byte) ((msb >> ((7 - i) * 8)) & 0xff);
         }
-        for (int i = 8; i < 16; i++) {
+        for (var i = 8; i < 16; i++) {
             out[i] = (byte) ((lsb >> ((15 - i) * 8)) & 0xff);
         }
         return out;
