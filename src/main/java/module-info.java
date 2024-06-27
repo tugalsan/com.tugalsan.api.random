a38d8801a43c7965508a5bab91e89f8ab08b7dfe
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-06-27 12:08:19 +0300

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,7 @@ module com.tugalsan.api.random {
     requires com.tugalsan.api.hex;
     requires com.tugalsan.api.shape;
     requires com.tugalsan.api.unsafe;
-    requires com.tugalsan.api.callable;
+    requires com.tugalsan.api.function;
     exports com.tugalsan.api.random.client;
     exports com.tugalsan.api.random.server;
 }
